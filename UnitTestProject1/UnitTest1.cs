a44b46928e43f99f28079853c32e4aaf1c6f528c
Mario Andres Ravina <ayarmarxio@gmail.com>
2018-09-27 11:15:46 +0200

    Done

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WeightConverterLib;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double ouncesValue = 1;
+            WeightConverterLibClass converter = new WeightConverterLibClass();
+            double result = converter.fromOuncesToGrams(ouncesValue);
+            Assert.AreEqual(28.35, result);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            double gramsValue = 1;
+            WeightConverterLibClass converter = new WeightConverterLibClass();
+            double result = converter.fromGramToOunces(gramsValue);
+            Assert.AreEqual(0.035274, result);
+
+        }
+    }
+}
---
+++ WeightConverterLib/WeightConverterLibClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeightConverterLib
+{
+    public class WeightConverterLibClass
+    {
+        private double ouncesConverter = 0.035274;
+        private double gramsConverter = 28.35;
+
+        public WeightConverterLibClass()
+        {
+
+        }
+
+        public double fromGramToOunces(double grams)
+        {
+            
+            double ounces = grams * ouncesConverter ;
+            return ounces;
+        }
+
+        public double fromOuncesToGrams(double ounces)
+        {
+            double grams = ounces * gramsConverter;
+            return grams;
+        }
+    }
+}
